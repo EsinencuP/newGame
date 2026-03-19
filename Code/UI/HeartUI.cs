cc0efd493a31597f4e5ffee4898559a58d053b39
lilDekim <esinencu_p@lyceum.ort.md>
2026-03-19 22:25:08 +0200

    new file:   Audio.meta 	new file:   Audio/jumping-on-a-metal-surface.mp3.meta 	new file:   Audio/jumping-on-hard-metal.mp3.meta 	new file:   Audio/shagi_po_metallu.mp3.meta 	new file:   Code.meta 	new file:   Code/Pick up.meta 	new file:   Code/Player.meta 	deleted:    Code/Player/PlayerFlip.cs 	deleted:    Code/Player/PlayerFlip.cs.meta 	modified:   Code/Player/movePlayer.cs 	new file:   Code/Traps.meta 	new file:   Code/UI.meta 	modified:   Code/UI/HeartUI.cs 	new file:   Concept.meta 	new file:   Concept/HP.jpg.meta 	new file:   Concept/O2_bar.jpg.meta 	new file:   Concept/player_anim1.jpg.meta 	new file:   Concept/player_anim2.jpg.meta 	new file:   Concept/player_anim3.jpg.meta 	new file:   Concept/player_anim4.jpg.meta 	new file:   Concept/player_anim5.jpg.meta 	new file:   Concept/spaceship1.jpg.meta 	new file:   Concept/spaceship2.jpg.meta 	new file:   Concept/spaceship3.jpg.meta 	new file:   Concept/spaceship4.jpg.meta 	new file:   Concept/spaceship5.jpg.meta 	new file:   Concept/tilemap1.jpg.meta 	new file:   Concept/tilemap2.jpg.meta 	new file:   Concept/tilemap3.jpg.meta 	new file:   Concept/tilemap4.jpg.meta 	new file:   Concept/tilemap5.jpg.meta 	new file:   "Concept/\320\240\320\260\320\267\320\272\320\260\320\264\321\200\320\276\320\262\320\272\320\260.jpg.meta" 	new file:   Texture.meta 	new file:   Texture/empty.txt.meta

--- Code/UI/HeartUI.cs
+++ Code/UI/HeartUI.cs
@@ -2,21 +2,29 @@ using UnityEngine;
 
 public class HeartUI : MonoBehaviour
 {
-    public PlayerHealth player; //* Ссылка на компонент PlayerHealth, который содержит информацию о здоровье игрока
-    public GameObject[] hearts; //* Массив объектов, представляющих сердца в UI
+    [SerializeField] private PlayerHealth player;
+    [SerializeField] private GameObject[] hearts;
 
-    void Update() //! Обновляем отображение сердец в UI в зависимости от текущего здоровья игрока
+    private void OnEnable()
     {
-        for (int i = 0; i < hearts.Length; i++) //? Проходим по каждому объекту сердца в массиве
+        player.OnHealthChanged += UpdateHearts;
+    }
+
+    private void OnDisable()
+    {
+        player.OnHealthChanged -= UpdateHearts;
+    }
+
+    private void Start()
+    {
+        UpdateHearts(player.CurrentHealth, player.MaxHealth);
+    }
+
+    private void UpdateHearts(int current, int max)
+    {
+        for (int i = 0; i < hearts.Length; i++)
         {
-            if (i < player.GetHealth()) //? Если индекс сердца меньше текущего здоровья игрока, отображаем его
-            {
-                hearts[i].SetActive(true); //? Активируем объект сердца, чтобы он был видимым в UI
-            }
-            else
-            {
-                hearts[i].SetActive(false); //  ? Деактивируем объект сердца, чтобы он не был видимым в UI
-            }
+            hearts[i].SetActive(i < current);
         }
     }
 }
